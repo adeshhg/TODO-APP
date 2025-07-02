7046a6029062ee5527bead90c430bb7ecd233264
adeshhg <adeshawm11@gmail.com>
2025-07-02 12:43:10 +0545

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTask() {
+    const textInput = document.getElementById("textInput").value
+
+    if (textInput == "") {
+        alert("Please enter a task")
+    }
+    else {
+adkfjhadskjf
+    }
+}
